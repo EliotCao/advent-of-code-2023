f52c7b90bc71f1d6d5fd042ae68a55c5e7d933ac
EliotCao <484900039@qq.com>
2023-12-06 15:09:30 +0800

    Add Day01-part2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -29,8 +29,48 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(output)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let output = input
+        .lines()
+        .map(|line| {
+            let mut it = (0..line.len()).filter_map(|index| {
+                let reduced_line = &line[index..];
+                let result = if reduced_line.starts_with("one") {
+                    Some(1)
+                } else if reduced_line.starts_with("two") {
+                    Some(2)
+                } else if reduced_line.starts_with("three") {
+                    Some(3)
+                } else if reduced_line.starts_with("four") {
+                    Some(4)
+                } else if reduced_line.starts_with("five") {
+                    Some(5)
+                } else if reduced_line.starts_with("six") {
+                    Some(6)
+                } else if reduced_line.starts_with("seven") {
+                    Some(7)
+                } else if reduced_line.starts_with("eight") {
+                    Some(8)
+                } else if reduced_line.starts_with("nine") {
+                    Some(9)
+                } else {
+                    reduced_line
+                        .chars()
+                        .next()
+                        .unwrap()
+                        .to_digit(10)
+                };
+                result
+            });
+            let first = it.next().expect("should be a number");
+
+            match it.last() {
+                Some(num) => first * 10 + num,
+                None => first * 10 + first,
+            }
+        })
+        .sum::<u32>();
+    Some(output)
 }
 
 #[cfg(test)]
@@ -46,6 +86,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(53221));
     }
 }
