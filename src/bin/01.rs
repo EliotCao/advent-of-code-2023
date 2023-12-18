f3fbb3a378d3ce6508ae3b879952cb0d3f558746
EliotCao <484900039@qq.com>
2023-12-18 09:14:06 +0800

    Fix Day01&Day02

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -80,12 +80,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(55834));
+        assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(53221));
+        assert_eq!(result, Some(281));
     }
 }
