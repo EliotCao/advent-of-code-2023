55cb71bbe7d76b0b8ea6b69663b388bd39b6b1a0
EliotCao <484900039@qq.com>
2023-12-23 10:22:37 +0800

    Add Day04-part1

---
+++ src/bin/04.rs
@@ -0,0 +1,53 @@
+advent_of_code::solution!(4);
+
+type Card<'a> = (Vec<u32>, Vec<u32>);
+
+fn parse_card(line: &str) -> Option<Card> {
+    line.split_once(':')?.1.split_once('|').map(|(a, b)| {
+        (
+            a.split_ascii_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+            b.split_ascii_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        )
+    })
+}
+
+fn count_hits(card: &Card) -> usize {
+    card.0.iter().filter(|c| card.1.contains(c)).count()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .filter_map(|l| {
+                let hits = count_hits(&parse_card(l)?) as u32;
+                Some(if hits < 2 { hits } else { 2_u32.pow(hits - 1) })
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
