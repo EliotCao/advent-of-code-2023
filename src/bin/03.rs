5272289af5923332fc3d024cfc95ce3e250c0553
EliotCao <484900039@qq.com>
2024-01-01 10:12:54 +0800

    Fix Day03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,10 +1,11 @@
-advent_of_code::solution!(3);
-
 use std::collections::VecDeque;
+
 use advent_of_code::helpers::matrix::{Cell, Direction, Matrix};
 
+advent_of_code::solution!(3);
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let matrix = Matrix::from(input);
+    let matrix: Matrix<char> = Matrix::from(input);
 
     let mut curr = String::new();
 
@@ -36,7 +37,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let matrix = Matrix::from(input);
+    let matrix: Matrix<char> = Matrix::from(input);
 
     let sum = matrix.items().fold(0, |mut acc, cell| {
         if cell.val == '*' {
@@ -130,13 +131,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
+    fn part_one_example() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(4361));
     }
 
     #[test]
-    fn test_part_two() {
+    fn part_two_example() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(467835));
     }
